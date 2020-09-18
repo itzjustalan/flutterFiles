e007939e5da88ba80720cfefe522f78dea622bd3
Alan <alanjohn165@gmail.com>
2020-09-18 15:03:23 +0530

    notification channel(kotlin)

---
+++ MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.deltra_fcm //Your own package name
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.net.Uri;
+import android.media.AudioAttributes;
+import android.content.ContentResolver;
+
+
+class MainActivity: FlutterActivity() {
+  private val CHANNEL = "uniquedeltrachannel.com/channel_test" //The channel name you set in your main.dart file
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      // Note: this method is invoked on the main thread.
+      call, result ->
+
+      if (call.method == "createNotificationChannel"){
+          @Suppress("UNCHECKED_CAST")//todo !!
+        val argData = call.arguments as java.util.HashMap<String, String>
+          val completed = createNotificationChannel(argData)
+          if (completed == true){
+              result.success(completed)
+          }
+          else{
+              result.error("Error Code", "Error Message", null)
+          }
+      } else {
+        result.notImplemented()
+      }
+    }
+
+  }
+
+    private fun createNotificationChannel(mapData: HashMap<String,String>): Boolean {
+        val completed: Boolean
+        if (VERSION.SDK_INT >= VERSION_CODES.O) {
+            // Create the NotificationChannel
+            val id = mapData["id"]
+            val name = mapData["name"]
+            val descriptionText = mapData["description"]
+            //val sound = "Ar_Alert"
+            val importance = NotificationManager.IMPORTANCE_MAX
+            val mChannel = NotificationChannel(id, name, importance)
+            mChannel.description = descriptionText
+
+            mChannel.canShowBadge()
+            mChannel.shouldVibrate()
+            mChannel.shouldShowLights()
+            mChannel.canBypassDnd()
+
+            //val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://"+ getApplicationContext().getPackageName() + "/raw/alert.wav");
+            //val att = AudioAttributes.Builder()
+            //.setUsage(AudioAttributes.USAGE_NOTIFICATION)
+            //.setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            //.build();
+
+            //mChannel.setSound(soundUri, att)
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+            completed = true
+        }
+        else{
+            completed = false
+        }
+        return completed
+    }
+}
